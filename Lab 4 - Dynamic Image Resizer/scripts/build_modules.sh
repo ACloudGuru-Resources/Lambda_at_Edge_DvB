9cb54493b2cb179d1bccbadc952c0c1b75fe5e2a
Dan Van Brunt <dvanbrunt@gmail.com>
2018-08-01 20:18:43 -0400

    feat(Lab4): Dynamic Image Tranformer

--- Lab 4 - Dynamic Image Resizer/scripts/build_modules.sh
+++ Lab 4 - Dynamic Image Resizer/scripts/build_modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build --tag amazonlinux:nodejs .
-docker run --rm --volume ${PWD}/functions/originResponse:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install sharp --save; npm install querystring --save; npm install --only=prod"
+docker run --rm --volume ${PWD}/functions/originResponse:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install sharp --save; npm install --only=prod"
 docker run --rm --volume ${PWD}/functions/viewerRequest:/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm init -f -y; npm install querystring --save; npm install --only=prod"
