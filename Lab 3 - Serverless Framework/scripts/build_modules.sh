13e47f0ded9d525dc64347cf4ecc39c003552621
Dan Van Brunt <dvanbrunt@gmail.com>
2018-08-12 11:06:02 -0400

    WIP

---
+++ Lab 3 - Serverless Framework/scripts/build_modules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build --tag amazonlinux:nodejs .
+docker run --rm --volume "${PWD}/functions/originResponse":/build amazonlinux:nodejs /bin/bash -c "source ~/.bashrc; npm install --only=prod"
